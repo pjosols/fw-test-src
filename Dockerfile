6321ddfa1e8a2ba9e0633121890ad7d541e05022
Paul Olsen <polsen@diligent.com>
2020-01-27 11:57:16 -0500

    update test to use requests

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,11 @@ FROM alpine:3.5
 RUN apk add --update py2-pip
 
 # upgrade pip
-# RUN pip install --upgrade pip
+RUN pip install --upgrade pip
+
+# install Python modules needed by the Python app
+COPY requirements.txt /tmp/
+RUN pip install --no-cache-dir -r /tmp/requirements.txt
 
 # copy files required for the app to run
 COPY test.py /opt/fw-tests/
--- test.py
+++ test.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python2.7
 
-import socket
+import requests
 
 
 for port in __list_of_ports__:
 
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('__destination_ip__', port))
-        s.sendall('Hello, world')
-        data = s.recv(1024)
-        s.close()
-        # print 'Received', repr(data)
-        message = "Pass"
+        r = requests.get('http://__destination_ip__:{}'.format(port), timeout=10)
+        message = "Success"
 
-    except socket.error as e:
-
-        # if "Connection refused" in e:
-        # not firewall blocking, but no service running
-
-        # if "No route to host" in e:
-        # server firewall is blocking
-
-        message = "Fail: {}".format(e)
+        if r.status_code != 200:
+            message = "Fail: {}".format(r.status_code)
 
     except Exception as e:
-        message = "Fail: {}".format(e)
 
-    else:
-        pass  # message = "Fail"
+        message = "Fail: {}".format(e)
 
     print "{} {}".format(port, message)
