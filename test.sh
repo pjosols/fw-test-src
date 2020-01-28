946c8ce6e45217451b4e35426cacdc25427fc16b
Paul Olsen <polsen@diligent.com>
2020-01-28 14:45:52 -0500

    remove commands

--- test.sh
+++ test.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-
-# upd
-nc -uzvv __destination_ip__ __list_of_udp_ports__
-
-# tcp
-nc -zvv __destination_ip__ __list_of_tcp_ports__
+# Ansible adds the necessary commands before the docker build
