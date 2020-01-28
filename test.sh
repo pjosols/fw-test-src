9800c489c8e04354d9c31451e9a623570c6d54ea
Paul Olsen <polsen@diligent.com>
2020-01-28 11:54:29 -0500

    use nc instead of nmap

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/ash
 
-# nmap -p 100,200,300,400,500 10.4.34.51
-nmap -p __list_of_ports__ __destination_ip__ --reason --host-timeout 10
+
+# upd
+# nc -uzvv 10.4.34.51 5005 5006 4444
+
+# tcp
+nc -zvv __destination_ip__ __list_of_ports__
