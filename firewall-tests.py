061ec0f1a4a3db72e9b88a99d6209a94b157f106
Paul Olsen <polsen@diligent.com>
2020-02-12 15:39:48 -0500

    fix case where udp or tcp ports are empty

--- firewall-tests.py
+++ firewall-tests.py
@@ -4,8 +4,8 @@ from os import environ
 # nc -zvv $DEST_IP $TCP_PORTS
 SRC_IP = environ.get("SRC_IP")
 DEST_IP = environ.get("DEST_IP")
-TCP_PORTS = environ.get("TCP_PORTS").split(" ")  # string like "100 200" becomes [100, 200]
-UDP_PORTS = environ.get("UDP_PORTS").split(" ")
+TCP_PORTS = environ.get("TCP_PORTS").split(" ") if environ.get("TCP_PORTS") else []  # string like "100 200" becomes [100, 200]
+UDP_PORTS = environ.get("UDP_PORTS").split(" ") if environ.get("UDP_PORTS") else []
 
 
 results = list()
@@ -17,11 +17,11 @@ for port in TCP_PORTS:
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
 
-    open = True if "open" in stdout or "bytes received" in stdout else False
+    open_ = True if "open" in stdout or "bytes received" in stdout else False
 
     results.append(
         "{},{},{},{},{},{}".format(
-            SRC_IP, DEST_IP, port, "TCP", open, stdout
+            SRC_IP, DEST_IP, port, "TCP", open_, stdout
         )
     )
 
@@ -32,11 +32,11 @@ for port in UDP_PORTS:
                            stderr=subprocess.STDOUT)
     stdout, stderr = out.communicate()
 
-    open = True if "open" in stdout or "bytes received" in stdout else False
+    open_ = True if "open" in stdout or "bytes received" in stdout else False
 
     results.append(
         "{},{},{},{},{},{}".format(
-            SRC_IP, DEST_IP, port, "UDP", open, stdout
+            SRC_IP, DEST_IP, port, "UDP", open_, stdout
         )
     )
 
